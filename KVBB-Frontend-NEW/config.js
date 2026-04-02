69b7a28f60dd000ef680ee7393f2c31d8ebdb831
JunChen0111414 <jun.chen@telekom.de>
2026-04-02 09:11:56 +0200

    Add files via upload

---
+++ KVBB-Frontend-NEW/config.js
@@ -0,0 +1,11 @@
+// ============================================================
+//  Konfiguration
+// ============================================================
+window.ENV = {
+    // Adresse des lokalen Python-Servers (server.py)
+    SERVER_URL: "https://kvbb-test-api-01-acddb3gkeqdhd9h2.westeurope-01.azurewebsites.net",
+
+    // n8n-Webhook-URL – wird serverseitig von server.py gelesen
+    N8N_URL: "https://mms-n8n.germanywestcentral.cloudapp.azure.com/webhook/cab2d150-f4ca-40cd-b1d3-7d792a979366"
+
+};
